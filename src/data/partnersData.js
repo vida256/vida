929faa8b6f2aecbaa983d13899e78177d093b64f
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-12-24 07:54:31 +0300

    new file:   public/partners/amfri.png 	new file:   public/partners/caritus.png 	new file:   public/partners/ceford.png 	new file:   public/partners/comboni.png 	new file:   public/partners/cream.png 	new file:   public/partners/fca.png 	new file:   public/partners/humanity.png 	new file:   public/partners/nrc.png 	new file:   public/partners/nssf.png 	new file:   public/partners/psfu.png 	new file:   public/partners/sepspel.png 	new file:   public/partners/unwomen.png 	new file:   public/partners/uweso.png 	modified:   src/data/partnersData.js

--- src/data/partnersData.js
+++ src/data/partnersData.js
@@ -1,24 +1,30 @@
 // Import partner logos
 import ACTIONAID from '/partners/actionaid.png';
+import AMFRI from '/partners/amfri.png';
 import CARE from '/partners/care.png';
+import CARITUS from '/partners/caritus.png';
+import CEFORD from '/partners/ceford.png';
 import CNOOC from '/partners/cnooc.png';
+import COMBONI from '/partners/comboni.png';
 import COMVIS from '/partners/comvis.png';
+import CREAM from '/partners/cream.png';
+import FCA from '/partners/fca.png';
 import GIZ from '/partners/giz.png';
 import GOAL from '/partners/goal.png';
+import HUMANITY from '/partners/humanity.png';
 import IFAD from '/partners/ifad.png';
 import IIRR from '/partners/iirr.png';
-// import MAAI from '/partners/maai.png';
 import MAAIF from '/partners/maaif.png';
 import MWE from '/partners/mwe.png';
+import NRC from '/partners/nrc.png';
+import NSSF from '/partners/nssf.png';
+import PSFU from '/partners/psfu.png';
 import RESTLESS from '/partners/restless.png';
+import SEPSPEL from '/partners/sepspel.png';
+import UNWOMEN from '/partners/unwomen.png';
+import UWESO from '/partners/uweso.png';
 
 export const partnersData = [
-//   {
-//     id: 1,
-//     name: "Ministry of Agriculture, Animal Industry and Fisheries",
-//     shortName: "MAAI",
-//     logo: MAAI
-//   },
   {
     id: 2,
     name: "Ministry of Agriculture, Animal Industry & Fisheries",
@@ -84,6 +90,84 @@ export const partnersData = [
     name: "Community Vision Uganda",
     shortName: "COMVIS",
     logo: COMVIS
+  },
+  {
+    id: 13,
+    name: "National Social Security Fund",
+    shortName: "NSSF",
+    logo: NSSF
+  },
+  {
+    id: 14,
+    name: "CREAM Westnile",
+    shortName: "CREAM",
+    logo: CREAM
+  },
+  {
+    id: 15,
+    name: "Fin Church Aid",
+    shortName: "FCA",
+    logo: FCA
+  },
+  {
+    id: 16,
+    name: "Humanity for Inclusion",
+    shortName: "HUMANITY",
+    logo: HUMANITY
+  },
+  {
+    id: 17,
+    name: "Caritus",
+    shortName: "CARITUS",
+    logo: CARITUS
+  },
+  {
+    id: 18,
+    name: "UN Women",
+    shortName: "UNWOMEN",
+    logo: UNWOMEN
+  },
+  {
+    id: 19,
+    name: "SEPSPEL",
+    shortName: "SEPSPEL",
+    logo: SEPSPEL
+  },
+  {
+    id: 20,
+    name: "Amfri Farms Limited",
+    shortName: "AMFRI",
+    logo: AMFRI
+  },
+  {
+    id: 21,
+    name: "Private Sector Foundation Uganda",
+    shortName: "PSFU",
+    logo: PSFU
+  },
+  {
+    id: 22,
+    name: "Uganda Women's Effort to Save Orphans",
+    shortName: "UWESO",
+    logo: UWESO
+  },
+  {
+    id: 23,
+    name: "Community Empowerment for Rural Development",
+    shortName: "CEFORD",
+    logo: CEFORD
+  },
+  {
+    id: 24,
+    name: "Norwegian Refugee Council",
+    shortName: "NRC",
+    logo: NRC
+  },
+  {
+    id: 25,
+    name: "Comboni Missionaries",
+    shortName: "COMBONI",
+    logo: COMBONI
   }
 ];
 
